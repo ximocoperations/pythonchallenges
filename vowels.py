3a66a95f835cb42077727ca49b782fff2da717fa
Kiranda Moses <ximocoperations@gmail.com>
2019-01-28 01:15:44 +0330

    pytho challenge solution

---
+++ vowels.py
@@ -0,0 +1,18 @@
+# get a word from my user
+word=input("Please enter any word :")
+vowel="aiouAIOU"
+
+count=0
+for i in word:
+	if i in vowel:
+		list=[]
+		for a in i:
+			list.append(i)
+			length=count+1
+
+list.append(length)	 
+
+
+
+
+
